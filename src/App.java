99e0015246a7c87558ec86f76b9e11be01c56789
LuisAlvesSilva <121822005+LuisAlvesSilva@users.noreply.github.com>
2023-03-24 21:30:07 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome = "Luis";
+        System.out.println(nome);
+        System.out.println("Tudo Bem?");
+    }
+}
