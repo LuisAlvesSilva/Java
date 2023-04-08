e601ab76a3bd7861ffe64abfe6f2851fd23f0e5a
LuisAlvesSilva <121822005+LuisAlvesSilva@users.noreply.github.com>
2023-04-08 19:50:42 -0300

    Add files via upload

---
+++ Exercicio_01_java/src/App.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // 1. Faça um Programa que mostre a mensagem "Alo Mundo" na tela.
+        /* System.out.println("Alo Mundo"); */
+
+        // 2. Faça um Programa que peça um número e então mostre a mensagem: O número
+        // informado é: [Numero]
+        /*
+         * Scanner entrada = new Scanner(System.in);
+         * int numero;
+         * 
+         * System.out.print("Digite seu numero: ");
+         * numero = entrada.nextInt();
+         * 
+         * System.out.printf("Seu numero é " + numero + "\n");
+         */
+
+        // 3. Faça um prigrama que pela dois números e imprima a soma
+        /*
+         * Scanner entrada = new Scanner(System.in);
+         * int numero_01;
+         * int numero_02;
+         * 
+         * System.out.print("Digite o seu primeiro numero: ");
+         * numero_01 = entrada.nextInt();
+         * 
+         * System.out.print("Digite o seu segundo numero: ");
+         * numero_02 = entrada.nextInt();
+         * 
+         * int soma;
+         * soma = numero_01 + numero_02;
+         * 
+         * System.out.printf("A soma dos seus numeros é: " + soma);
+         */
+
+        // 4. Faça um programa que peça as 4 notas bimestrais e mostra a media.
+        /*
+         * Scanner entrada = new Scanner(System.in);
+         * float nota_01;
+         * float nota_02;
+         * float nota_03;
+         * float nota_04;
+         * 
+         * System.out.print("Digite a sua primeira nota: ");
+         * nota_01 = entrada.nextFloat();
+         * System.out.print("Digite a sua segunda nota: ");
+         * nota_02 = entrada.nextFloat();
+         * System.out.print("Digite a sua terceira nota: ");
+         * nota_03 = entrada.nextFloat();
+         * System.out.print("Digite a sua quarta nota: ");
+         * nota_04 = entrada.nextFloat();
+         * 
+         * float media;
+         * media = (nota_01 + nota_02 + nota_03 + nota_04) / 4;
+         * 
+         * System.out.printf("A sua media foi: " + media);
+         */
+
+        // 5. Faça um programa que converta metros para centimetros.
+
+    }
+}
