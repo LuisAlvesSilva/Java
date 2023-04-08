104565b8c1bfb7101c11670503bb4bf755750034
LuisAlvesSilva <121822005+LuisAlvesSilva@users.noreply.github.com>
2023-04-08 19:51:42 -0300

    Update App.java

--- Exercicio_01_java/src/App.java
+++ Exercicio_01_java/src/App.java
@@ -61,5 +61,46 @@ public class App {
 
         // 5. Faça um programa que converta metros para centimetros.
 
+
+        // 6. Faça um programa que peça o raio de um circulo, calcule e mostre sua area.
+
+        // 7. Faça um programa e calcule a area de um quadrado, em seguida mostro o doro desta area para o usuario.
+
+        // 8. Faça um programa que pergunte quanto voce ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+        // 9. Faça um programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+
+        // 10. Faça um programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+
+        // 11. Faça um programa que peça 2 números inteiros e um número real. Calcule e mostre: a. o produto do dobro do primeiro com a metado do
+            //a. o produto do dobro do primeiro com a metado do
+            //b. a soma do triplo do primeiro com o terceiro.
+            //c. o terceiro elevado ao cubo.
+
+        // 12. Tendo como dados de entrada a altura de uma pessoa, contrua um algoritmo que calcule seu peso ideal, usando a seguinte formula: (72.7*altura)-58
+
+        // 13. Tendo como dado de entrada a altura (h) de uma pessoa, contrua um algoritmo que calcule seu peso ideal, utilizando as seguintes formulas:
+            //Para Homens: (72.7*h) - 58
+            //Para Mulheres: (62.1*h) - 44.7
+
+        // 14. João Papo-de-Pescador, homen de bem, comprou um mocrocomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+
+        // 15. Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+            //a. Salário Bruto
+            //b. Quanto pagou ao Imposto de Renda
+            //c. Quanto pagou ao INSS
+            //d. Quanto pagou ao Sindicato
+            //e. O Salário Líquido
+
+        // 16. Faça um programa para um loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura de tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidade de latas de tinta a serem compreadas e o preço total.
+
+        // 17. Faça um programa para um loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura de tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam 80,00 reais ou em galões de 3,6 litros, que custam 25,00 reais.
+            //Informe ao usuário as qunatidades de tinta a serem compradas e os respectivos preços em 3 situações:
+            //Comprar apenas latas de 18 litros;
+            //Comprar apenas galões de 3,6 litros;
+            //misturar as latas e galões, de forma que o preço seja o menos. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
+
+        // 18. Faça um programa que peça o tamanho de um arquivo de download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+        
     }
 }
